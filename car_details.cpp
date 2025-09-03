fdc43ec3d6dcfaaa05246a7c1fc52ab2004e60a3
ananyakudtarkar <ananyask.03@gmail.com>
2025-09-03 19:32:00 +0530

    Create car_details.cpp

---
+++ car_details.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+public:
+    string brand;
+    int year;
+
+    void input() {
+        cout << "Enter car brand: ";
+        cin >> brand;
+        cout << "Enter manufacturing year: ";
+        cin >> year;
+    }
+
+    void display() {
+        cout << "Car Brand: " << brand << endl;
+        cout << "Manufacturing Year: " << year << endl;
+    }
+};
+
+int main() {
+    Car c1;
+    c1.input();
+    c1.display();
+    return 0;
+}
+
+/*
+OUTPUT
+Enter car brand: Renault 
+Enter manufacturing year: 2014
+Car Brand: Toyota
+Manufacturing Year: 2006
+*/
